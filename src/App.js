059d5651c2f58367d1afa481df777be754199bbb
yunseop-dev <c.o.d.e@kakao.com>
2020-09-22 20:07:40 +0900

    1. useMemo 예제

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
-  return <div>Hello World</div>;
+const plusFive = (num) => {
+  console.log("i was called!");
+  return num + 5;
+};
+
+export default function App() {
+  const [num, setNum] = useState(0);
+  const [light, setLight] = useState(true);
+  const numPlusFive = plusFive(num);
+  return (
+    <div className={light ? "light" : "dark"}>
+      <div>
+        <h1>Without useMemo</h1>
+        <h2>
+          Current number: {num}, Plus five: {numPlusFive}
+        </h2>
+        <div className="button-container">
+          <button
+            onClick={() => {
+              setNum(num + 1);
+            }}
+          >
+            Update Number{" "}
+          </button>
+          <button
+            onClick={() => {
+              setLight(!light);
+            }}
+          >
+            {" "}
+            Toggle the light{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default App;
